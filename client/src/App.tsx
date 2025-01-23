c1117cd81f841232fed4d9463b2b095e11ff4a35
Ryan Dalal <ryanadalal@gmail.com>
2025-01-23 20:01:49 +0700

    started practicing with react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const stuff = [
+  { id: 1, name: "red", age: 10 },
+  { id: 2, name: "blue", age: 15 },
+  { id: 3, name: "green", age: 20 },
+];
 
+function MyButton(color: string, onClick: () => null) {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <button
+      className={
+        (color.id == 1 ? "bg-amber-600" : "bg-blue-600") +
+        " text-slate-800 flex w-full items-center rounded-md p-3 transition-all hover:bg-slate-100 focus:bg-slate-100 active:bg-slate-100"
+      }
+      key={color.id}
+      onClick={() => handle(color.id)}
+    >
+      {color.name + " " + color.age}
+    </button>
+  );
 }
 
-export default App
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handle(id: number) {
+    setCount(id);
+  }
+
+  const listItems = stuff.map((color) => <MyButton />);
+
+  return (
+    <div>
+      <div className="flex w-96 flex-col gap-1 p-1.5 relative rounded-lg bg-white shadow-sm border border-slate-200">
+        {listItems}
+      </div>
+      <button className="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2">
+        Press me! {count}
+      </button>
+    </div>
+  );
+}
