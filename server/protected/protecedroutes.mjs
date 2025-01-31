e40a6af8f9e81c6f1dacf9093706c34ef61da6c4
Ryan Dalal <ryanadalal@gmail.com>
2025-01-31 15:39:35 +0700

    minor change to protected route

--- server/protected/protecedroutes.mjs
+++ server/protected/protecedroutes.mjs
@@ -5,6 +5,7 @@ import { protectedAuthCallBackSuccess } from "./protectedcontroll.mjs";
 // define a router with the neccessary oauth routes
 const router = express.Router();
 
-router.get("/callback/success", authenticateJWT, protectedAuthCallBackSuccess);
+router.use(authenticateJWT);
+router.get("/callback/success", protectedAuthCallBackSuccess);
 
 export default router;
--- server/server.mjs
+++ server/server.mjs
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: CLIENT_URL, // Your frontend URL
+    origin: CLIENT_URL,
     credentials: true, // Allows sending cookies
   })
 );
