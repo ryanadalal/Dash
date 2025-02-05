6984eedbf2a850f459d72b04dfc89a2db74e77f7
Ryan Dalal <ryanadalal@gmail.com>
2025-02-05 21:08:10 +0700

    working on nav bar

--- client/src/views/home/Dashboard.tsx
+++ client/src/views/home/Dashboard.tsx
@@ -4,14 +4,43 @@ import Tile from "./Tile.tsx";
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-col h-screen bg-bg-slate group md:s-size">
+    <div className="flex flex-col h-screen bg-bg-slate md:s-size">
       <NavBar />
       <Body>
         <Tile class="min-w-60 lg:row-span-7 lg:col-span-2 hidden lg:block">
           <div></div>
         </Tile>
         <Tile class="min-w-140 row-span-5 lg:col-span-5 rounded-b-xl">
-          <div></div>
+          <div>
+            aaaaaafillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfillfillfillfillfillfill
+            fillfillfillfillfillfillfillfillfillfill
+          </div>
         </Tile>
         <Tile class="min-w-140 row-span-2 lg:col-start-3 col-span-5 rounded-t-xl">
           <div></div>
--- client/src/views/support/NavBar.tsx
+++ client/src/views/support/NavBar.tsx
@@ -1,37 +1,76 @@
 import { useSelector } from "react-redux";
 
 import { User } from "../../types/user-types.ts";
+import { ModalRendererProps } from "../../types/util-types.ts";
 
-function NavBarList({ text }: { text: string }) {
+function NavBarList(props: ModalRendererProps) {
   return (
-    <li className="flex items-center text-sm p-2 gap-x-2 text-textslate rounded-sm hover:bg-realamber">
+    <li className="relative group/nav flex items-center text-sm p-2 gap-x-2 text-textslate rounded-sm hover:bg-realamber">
       <a href="#" className="flex items-center">
-        {text}
+        {props.title}
       </a>
+      <div className="hidden group-hover/nav:block absolute top-full -right-10 z-[999] mt-2 w-250 rounded-xl border border-bgslate bg-white p-3 text-sm text-blue-gray-500 shadow-lg shadow-textslate/10">
+        {props.children}
+      </div>
     </li>
   );
 }
 function PhotoList({ photo }: { photo: string }) {
   return <img src={photo} className="h-10 w-10 rounded-full" />;
 }
+function NavDropDownElement() {
+  return (
+    <a href="#">
+      <button className="flex w-full cursor-pointer select-none items-center gap-3 rounded-lg px-3 pb-2 pt-[9px] text-start leading-tight outline-none transition-all hover:bg-bgslate focus:bg-realamber focus:text-black">
+        <div className="flex items-center justify-center rounded-lg p-2 ">
+          <img className="h-6 w-6" />
+        </div>
+        <div className="focus:text-inherit">
+          <h6 className="flex items-center font-sans text-sm font-bold tracking-normal text-black antialiased">
+            Products
+          </h6>
+          <p className="block font-sans text-xs !font-medium text-textslate focus:text-inherit antialiased">
+            Find the perfect solution for your needs.
+          </p>
+        </div>
+      </button>
+    </a>
+  );
+}
+
+//https://www.material-tailwind.com/docs/html/mega-menu
 
 export default function NavBar() {
   const photo = useSelector((state: User) => state.photo);
   return (
-    <nav className="flex flex-wrap h-20 sticky items-center justify-between w-full px-4 py-2 bg-white shadow-md rounded-b-xl lg:px-30 lg:py-3">
-      <a
-        href="#"
-        className="mr-4 block cursor-pointer py-1.5 text-xl text-realamber font-semibold"
-      >
-        Reals
-      </a>
-      <ul className="flex gap-1 mb-0 mt-0 flex-row items-center sm:gap-6">
-        <NavBarList text="Help"></NavBarList>
-        <NavBarList text="Verify"></NavBarList>
-        <NavBarList text="Accounts"></NavBarList>
-        <NavBarList text="Settings"></NavBarList>
-        {photo != undefined ? <PhotoList photo={photo}></PhotoList> : null}
-      </ul>
-    </nav>
+    <>
+      <nav className="flex flex-wrap h-20 sticky items-center justify-between w-full px-4 py-2 bg-white shadow-md rounded-b-xl lg:px-30 lg:py-3">
+        <a
+          href="#"
+          className="mr-4 block cursor-pointer py-1.5 text-xl text-realamber font-semibold"
+        >
+          Reals
+        </a>
+        <ul className="flex gap-1 mb-0 mt-0 flex-row items-center sm:gap-6">
+          <NavBarList title="Help">
+            <ul className="grid grid-cols-3 gap-y-2 outline-none outline-0">
+              <NavDropDownElement />
+              <NavDropDownElement />
+              <NavDropDownElement />
+              <NavDropDownElement />
+              <NavDropDownElement />
+              <NavDropDownElement />
+              <NavDropDownElement />
+              <NavDropDownElement />
+              <NavDropDownElement />
+            </ul>
+          </NavBarList>
+          <NavBarList title="Verify"></NavBarList>
+          <NavBarList title="Accounts"></NavBarList>
+          <NavBarList title="Settings"></NavBarList>
+          {photo != undefined ? <PhotoList photo={photo}></PhotoList> : null}
+        </ul>
+      </nav>
+    </>
   );
 }
